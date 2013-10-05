52c9516e3c4dc4bac95861b1817628543f1fdf49
Vinayak <vinayak11118@iiitd.ac.in>
2013-10-05 23:56:47 +0530

    input from files stored to byte array

--- src/AES.java
+++ src/AES.java
@@ -1,9 +1,22 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class AES {
-    public static void main(String[] args) {
+
+    private final int Nb = 4;
+    private final int word = 4;
+    private int Nk;
+    private int Nr;
+    private byte[] key;
+    private byte[][] w;
+    private byte[][] dw;
+    private byte[][] state;
+    private byte[][] rcon;
+
+    public static void readInput(String[] args) {
+
         if (args.length < 3) {
             System.err.println("\tUsage: AES option keyFile inputFile");
             System.err.println("\t\toption     [e]ncryption or [d]ecryption");
@@ -11,18 +24,25 @@ public class AES {
             System.err.println("\t\tinputFile  plain/cipher text (32 hex characters per line)\n");
             System.exit(1);
         }
-        /*
-            read CMD arguments
-         */
+
         String _mode = args[0];
         try {
             FileInputStream _keyFile = new FileInputStream(args[1]);
             FileInputStream _inputFile = new FileInputStream(args[2]);
         } catch (FileNotFoundException e) {
-            System.err.println("\tThe specified file was not found!");
             e.printStackTrace();
         }
     }
 
+    public static void setupVar() {
 
+    }
+
+    public static void main(String[] args) {
+        byte b[] = utils.hexToByte("00112233445566778899AABBCCDDEEFF");
+
+        for(int i=0; i<b.length; i++){
+            System.out.print(String.format("0x%02X", b[i]) + ",");
+        }
+    }
 }
