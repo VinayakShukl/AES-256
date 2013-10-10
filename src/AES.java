3d17595ec6ca1ef5c455a79ea5349c45115cd7bd
Vinayak <vinayak11118@iiitd.ac.in>
2013-10-10 20:09:16 +0530

    output formatted as per AED standard

--- src/AES.java
+++ src/AES.java
@@ -6,12 +6,15 @@ public class AES {
     private static FileInputStream _keyFile;
     private static FileInputStream _inputFile;
 
+    /*
+        all sizes are in terms of 'bytes'
+     */
     private final int Nb = 4;
-    private final int WORD = 4;
+    private final int wordSize = 4;
     private int Nk;
     private int Nr;
-    private byte[][] w;
-    private byte[][] dw;
+    private byte[][] eSchedule;
+    private byte[][] dschedule;
     private byte[][] state;
     private byte[][] rcon;
     private static byte[] input;
@@ -19,13 +22,13 @@ public class AES {
 
     public static void readArgs(String[] args) {
 
-        if (args.length < 3 ) {
+        if (args.length < 3) {
             System.err.println("\tUsage: AES option keyFile inputFile");
             System.err.println("\t\toption     [e]ncryption or [d]ecryption");
             System.err.println("\t\tkeyFile    file containing 256 bit key (64 hex characters, one line)");
             System.err.println("\t\tinputFile  plain/cipher text (32 hex characters per line)\n");
             System.exit(1);
-        } else if(args[0].length() != 1){
+        } else if (args[0].length() != 1) {
             System.err.println("\tUsage: AES option keyFile inputFile");
             System.err.println("\t\toption     [e]ncryption or [d]ecryption");
             System.exit(1);
@@ -64,15 +67,18 @@ public class AES {
                 break;
         }
         System.out.println("\nKEY LENGTH: " + Nk);
+        System.out.println("ROUNDS    : " + Nr);
         this.key = key;
-        //TODO: keyExpansion();
+        eSchedule = new byte[Nb*(Nr+1)][4];
+        dschedule = new byte[Nb*(Nr+1)][4];
+        //TODO: keyExpansion(_mode);
     }
 
-    public byte[] encrypt (byte[] input) {
+    public byte[] encrypt(byte[] input) {
         this.state = new byte[4][Nb];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < Nb; j++) {
-                this.state[j][i] = input[4*i+j];
+                this.state[j][i] = input[4 * i + j];
             }
         }
         printState("INITIAL STATE", this.state);
@@ -98,13 +104,13 @@ public class AES {
         input = utils.hexToByte(s2.toString());
     }
 
-    public void printState(String id, byte[][]b) {
+    public void printState(String id, byte[][] b) {
         long streamPtr = 0;
         System.out.println("\n" + id + ": ");
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < Nb; j++)  {
+            for (int j = 0; j < Nb; j++) {
                 System.out.print(String.format("0x%02X", b[i][j]) + " ");
-                this.state[j][i] = input[4*i+j];
+                this.state[j][i] = input[4 * i + j];
             }
             System.out.printf("\n");
         }
@@ -113,14 +119,24 @@ public class AES {
     public static void printInputs(String id, byte[] b, final int numberOfColumns) {
         long streamPtr = 0;
         System.out.println("\n" + id + ": ");
-        for (int k = 0; k < b.length; k++) {
-            final long col = streamPtr++ % numberOfColumns;
-            System.out.print(String.format("0x%02X", b[k]) + " ");
-            if (col == (numberOfColumns - 1)) {
-                System.out.printf("\n");
+
+        if (id.equals("INPUT")) {
+            for (int k = 0; k < b.length; k++) {
+                final long col = streamPtr++ % numberOfColumns;
+                System.out.print(String.format("0x%02X", b[k]) + " ");
+                if (col == (numberOfColumns - 1)) {
+                    System.out.printf("\n");
+                }
+            }
+        } else if (id.equals("KEY")) {
+            for (int k = 0; k < 4; k++) {
+                for (int i = 0; i < b.length / 4; i++)
+                    System.out.print(String.format("0x%02x ", b[i * 4 + k]));
+                System.out.println();
             }
         }
     }
+
 
     public static void main(String[] args) throws IOException {
         readArgs(args);
@@ -128,9 +144,9 @@ public class AES {
         printInputs("KEY", key, 8);
         printInputs("INPUT", input, 16);
         AES test = new AES(key);
-        if(_mode.charAt(0) == 'e')
+        if (_mode.charAt(0) == 'e')
             test.encrypt(input);
-        else{
+        else {
             System.err.println("Decryption not supported yet.");
             // TODO: test.decrypt(input);
         }
